6de85b338ffd6af6cff3361d1cb31ed0fb1ab48c
BenjaminEspinozaV <evb0034820@est.univalle.edu>
2026-05-25 11:16:42 -0400

    Update mailer.js

--- config/mailer.js
+++ config/mailer.js
@@ -1,15 +1,19 @@
-// config/mailer.js
-import nodemailer from "nodemailer";
 import dotenv from "dotenv";
-export const mailFrom = process.env.SMTP_FROM;
 dotenv.config();
+
+import nodemailer from "nodemailer";
+
+export const mailFrom = process.env.SMTP_FROM;
 
 export const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: false, // porque usas 587
+  secure: Number(process.env.SMTP_PORT) === 465,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  connectionTimeout: 30000,
+  greetingTimeout: 15000,
+  socketTimeout: 60000,
 });
